ba6cbeca0973c463b6f40fc057863f4be4aed22f
Rinat&Vanya <zazubaidullin@gmail.ru>
2016-11-18 14:26:18 +0300

    Added class Context

---
+++ Cars.Data/Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars.Data
+{
+    class Context : DbContext
+    {
+        public DbSet<Manufacturer> Manufacturers { get; set; }
+        public DbSet<Model> Models { get; set; }
+        public DbSet<Car> C { get; set; }
+
+        public Context() : base("localsql")
+        {
+        }
+
+    }
+}
